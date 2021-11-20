63fed2f89fcfa81478c790b702f5ebdde299c9eb
Anthony Steven Quito Penadillo <anthony.quito@tecsup.edu.pe>
2021-11-19 19:34:42 -0500

    first commit

---
+++ src/keys.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+    database: {
+        connectionLimit: 10,
+        host: 'demo-version.cnarbccogeum.us-east-1.rds.amazonaws.com',
+        user: 'admin',
+        password: '12345678',
+        database: 'contactos'
+    }
+
+};
